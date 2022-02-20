1a94c9ae572e3f8901d3dd20b6311214465bba4b
damirmeng <790430354@qq.com>
2022-02-20 19:59:43 +0800

    优化代码

--- public/nav.js
+++ public/nav.js
@@ -44,78 +44,102 @@ function writeMobileNav() {
 var width = document.documentElement.clientWidth;
 var container = document.querySelector('#container');
 
-var str = [ //导航栏一级文字
-    "首页",
-    "知海拾贝",
-    "我的原创",
-    "查找文章",
-    "关于",
-    "发现更多"
-]
+// 导航栏菜单信息
+var secondMenuStr = [
+    //** ------------------------一级菜单--------------
+    [
+        [ //导航栏一级文字
+            "首页",
+            "知海拾贝",
+            "我的原创",
+            "查找文章",
+            "关于",
+            "发现更多"
+        ],
+        [ //导航栏一级链接 
+            "../../../../homePage.html",
+            "javascript:void(0);",
+            "javascript:void(0);",
+            "../../../../about/search.html",
+            "../../../../about",
+            "javascript:void(0);"
+        ]
+    ],
+    //** ------------------------二级菜单--------------
+    [
+        // 第二个
+        [
+            "经典文章",
+            "技术文章",
+            "美句收录"
+        ],
+        [ //链接
+            "../../../../homePage.html#article",
+            "../../../../homePage.html#knowledge",
+            "../../../../sentence"
+        ]
+    ],
+    [
+        // 第三个
+        [
+            "传统文章",
+            "技术",
+            "个人句子",
+            "我的说说"
+        ],
+        [ //链接
+            "../../../../homePage.html#myArticle",
+            "../../../../homePage.html#myKnowledge",
+            "../../../../sentence/mySentence.html",
+            "../../../../about/saySomething.html"
+        ]
 
+    ],
+    [
+        [
+            "分类",
+            "标签",
+            "存档",
+            "搜索"
+        ],
+        [
+            "../../../../about/classification.html",
+            "../../../../about/tags.html",
+            "../../../../about/file.html",
+            "../../../../about/search.html#我的",
+        ]
 
-// 第2项二级标题
-var strList2 = [
-    "经典文章",
-    "技术文章",
-    "美句收录"
-]
-var hrefList2 = [ //链接
-    "../../../../homePage.html#article",
-    "../../../../homePage.html#knowledge",
-    "../../../../sentence"
-]
-// 第3项二级标题
-var strList3 = [
-    "传统文章",
-    "技术",
-    "个人句子",
-    "我的说说"
-]
-var hrefList3 = [ //链接
-    "../../../../homePage.html#myArticle",
-    "../../../../homePage.html#myKnowledge",
-    "../../../../sentence/mySentence.html",
-    "../../../../about/saySomething.html"
-]
-// 第4项二级标题
-var strList4 = [
-    "分类查找",
-    "标签",
-    "存档",
-    "搜索"
-]
-var hrefList4 = [
-    "../../../../about/classification.html",
-    "../../../../about/tags.html",
-    "../../../../about/file.html",
-    "../../../../about/search.html#我的",
-]
-// 第5项二级标题
-var strList5 = [
-    "关于本站",
-    "更新日志",
-    "本站统计",
-    "本站概述",
-    "留言板"
-]
-var hrefList5 = [
-    "../../../../about",
-    "../../../../about/timeline.html",
-    "https://tongji.baidu.com/web/welcome/ico?s=dfb2e9af2c4ea3536c96e73ddb3dc6b8",
-    "../../../../about/summary.html",
-    "../../../../about/guestbook.html"
-]
-// 第6项二级标题
-var strList6 = [
-    "首个网站",
-    "每日明记",
-    "计时器"
-]
-var hrefList6 = [
-    "https://mdming.github.io",
-    "../../../../diary",
-    "../../../../tools/timer"
+    ],
+    [
+        [
+            "关于本站",
+            "更新日志",
+            "本站统计",
+            "本站概述",
+            "留言板"
+        ],
+        [
+            "../../../../about",
+            "../../../../about/timeline.html",
+            "https://tongji.baidu.com/web/welcome/ico?s=dfb2e9af2c4ea3536c96e73ddb3dc6b8",
+            "../../../../about/summary.html",
+            "../../../../about/guestbook.html"
+        ]
+
+    ],
+    [
+        [
+            "首个网站",
+            "每日明记",
+            "计时器"
+        ],
+        [
+            "https://damirmeng.github.io/oldBlog",
+            "../../../../diary",
+            "../../../../tools/timer"
+        ]
+    ]
+
 ]
 
 function setNav() { //导航栏设置
@@ -124,23 +148,7 @@ function setNav() { //导航栏设置
         writeMobileNav();
         writeMobileTopBar();
 
-        // 监听菜单键
-        document.querySelector(".menubar").addEventListener("click", // 手机端菜单键点击后执行的函数
-            function () {
-                if (document.querySelector(".menubar").classList.contains('arrow')) {
-                    document.querySelector(".menubar").classList.remove("arrow");
-                    document.querySelector(".nav-container").style.display = "none";
-                } else {
-                    document.querySelector(".menubar").classList.add("arrow");
-                    document.querySelector(".nav-container").style.display = "block";
-                }
-            })
-
-        while (document.querySelectorAll(".mobile-navbar ul li").length < str.length) { //li的数量为6
-            var last = document.querySelectorAll(".mobile-navbar ul li")[0].cloneNode(true);
-            document.querySelector(".mobile-navbar ul").appendChild(last);
-        }
-        var href = [ //链接 
+        secondMenuStr[0][1] = [ //一级菜单链接优化，防止点击打不开按钮 
             "../../../../homePage.html",
             "javascript:void(0);",
             "javascript:void(0);",
@@ -148,90 +156,51 @@ function setNav() { //导航栏设置
             "javascript:void(0);",
             "javascript:void(0);"
         ]
+
+        // 添加一级目录
         var mobileNavbar = document.querySelectorAll(".mobile-navbar ul li")
-        for (let index = 0; index < mobileNavbar.length; index++) {
-            //为每个li设置文字与链接
-            const element = mobileNavbar[index];
-            element.querySelector("a").innerHTML = str[index];
-            element.querySelector("a").href = href[index];
-        }
-
-        // 第2个的展开页
-        var ul = document.querySelector(".mobile-navbar ul")
-        var article_li = ul.children[1]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "second-menu");
-        for (let index = 0; index < strList2.length; index++) {
-            //由str的长度添加<li><a></a></li>
-            //并设置文字  href
-            var a = document.createElement("a");
-            a.innerHTML = strList2[index];
-            a.href = hrefList2[index];
-            var li1 = document.createElement("li");
-            li1.appendChild(a);
-            ul2.appendChild(li1);
-        }
-        article_li.appendChild(ul2);
-
-        // 第3个的展开页
-        var article_li = ul.children[2]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "second-menu")
-        for (let index = 0; index < hrefList3.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList3[index]
-            a.href = hrefList3[index]
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
-        }
-        article_li.appendChild(ul2)
-
-        // 第4个的展开页
-        var article_li = ul.children[3]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "second-menu")
-        for (let index = 0; index < strList4.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList4[index]
-            a.href = hrefList4[index]
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
-        }
-        article_li.appendChild(ul2)
-
-        // 第5个的展开页
-        var article_li = ul.children[4]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "second-menu")
-        for (let index = 0; index < strList5.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList5[index]
-            a.href = hrefList5[index]
-            if (strList5[index] == "本站统计") {
-                a.target = "_blank";
+        for (let index = 0; mobileNavbar.length < secondMenuStr[0][0].length; index++) { //为每个一级目录设置文字与链接
+            if (index == 0) { //第一个设置
+                mobileNavbar[0].querySelector("a").innerHTML = secondMenuStr[0][0][index]
+                mobileNavbar[0].querySelector("a").href = secondMenuStr[0][1][index]
+            } else { //其他克隆
+                var li = mobileNavbar[0].cloneNode(true);
+                li.querySelector("a").innerHTML = secondMenuStr[0][0][index]
+                li.querySelector("a").href = secondMenuStr[0][1][index]
+                document.querySelector(".mobile-navbar ul").appendChild(li);
             }
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
+            mobileNavbar = document.querySelectorAll(".mobile-navbar ul li")
         }
-        article_li.appendChild(ul2)
 
-        // 第6个的展开页
-        var article_li = ul.children[5]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "second-menu")
-        for (let index = 0; index < strList6.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList6[index]
-            a.href = hrefList6[index]
-            a.target = "_blank"; //新页面
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
+        // 从第2个一级菜单开始的展开页（二级菜单），第一个为首页，不设置
+        for (let index = 1; index < secondMenuStr.length; index++) {
+            const element = secondMenuStr[index];
+            var ul = document.querySelector(".mobile-navbar ul")
+            var article_li = ul.children[index]
+            var ul2 = document.createElement("ul");
+            ul2.setAttribute("class", "second-menu")
+            for (let index = 0; index < element[0].length; index++) {
+                var a = document.createElement("a");
+                a.innerHTML = element[0][index]
+                a.href = element[1][index]
+                var li1 = document.createElement("li");
+                li1.appendChild(a)
+                ul2.appendChild(li1)
+            }
+            article_li.appendChild(ul2)
         }
-        article_li.appendChild(ul2)
+
+        // 监听菜单键，点击动画
+        document.querySelector(".menubar").addEventListener("click", function () {
+            // 手机端菜单键点击后执行的函数
+            if (document.querySelector(".menubar").classList.contains('arrow')) {
+                document.querySelector(".menubar").classList.remove("arrow");
+                document.querySelector(".nav-container").style.display = "none";
+            } else {
+                document.querySelector(".menubar").classList.add("arrow");
+                document.querySelector(".nav-container").style.display = "block";
+            }
+        })
 
         //移动端一级菜单点击展开二级菜单
         window.onload = function (params) {
@@ -255,106 +224,41 @@ function setNav() { //导航栏设置
         }
     } else { //PC端导航栏
 
-        //设置导航栏文字
+        //设置导航栏HTML 
         writePCNav();
 
-        var href = [ //链接重新设置 
-            "../../../../homePage.html",
-            "javascript:void(0);",
-            "javascript:void(0);",
-            "../../../../about/search.html",
-            "../../../../about",
-            "javascript:void(0);"
-        ]
-
-        while (document.querySelectorAll(".tabbed ul li").length < str.length) { //li的数量为6
-            var last = document.querySelectorAll(".tabbed ul li")[0].cloneNode(true);
-            document.querySelector(".tabbed ul").appendChild(last);
-        }
-
-        var tabbed = document.querySelectorAll(".tabbed ul li")
-        for (let index = 0; index < tabbed.length; index++) {
-            //为每个li设置文字与链接
-            const element = tabbed[index];
-            element.querySelector("a").innerHTML = str[index]
-            element.querySelector("a").href = href[index]
-        }
-
-        // 第2个的展开页
-        var ul = document.querySelector(".tabbed ul")
-        var article_li = ul.children[1]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "ul_c")
-        for (let index = 0; index < strList2.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList2[index]
-            a.href = hrefList2[index]
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
-        }
-        article_li.appendChild(ul2)
-
-        // 第3个的展开页
-        var article_li = ul.children[2]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "ul_c")
-        for (let index = 0; index < strList3.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList3[index]
-            a.href = hrefList3[index]
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
-        }
-        article_li.appendChild(ul2)
-
-        // 第4个的展开页
-        var article_li = ul.children[3]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "ul_c")
-        for (let index = 0; index < strList4.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList4[index]
-            a.href = hrefList4[index]
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
-        }
-        article_li.appendChild(ul2)
-
-        // 第5个的展开页
-        var article_li = ul.children[4]
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "ul_c")
-        for (let index = 0; index < strList5.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList5[index]
-            a.href = hrefList5[index]
-            if (str[index] == "本站统计") {
-                a.target = "_blank";
+        // 添加一级目录
+        var tabbed = document.querySelectorAll(".tabbed ul li");
+        for (let index = 0; tabbed.length < secondMenuStr[0][0].length; index++) { //为每个一级目录设置文字与链接
+            if (index == 0) { //第一个设置
+                tabbed[0].querySelector("a").innerHTML = secondMenuStr[0][0][index]
+                tabbed[0].querySelector("a").href = secondMenuStr[0][1][index]
+            } else { //其他克隆
+                var li = tabbed[0].cloneNode(true);
+                li.querySelector("a").innerHTML = secondMenuStr[0][0][index]
+                li.querySelector("a").href = secondMenuStr[0][1][index]
+                document.querySelector(".tabbed ul").appendChild(li);
             }
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
+            tabbed = document.querySelectorAll(".tabbed ul li"); //更新
         }
-        article_li.appendChild(ul2)
 
-        // 第6个的展开页
-        var article_li = ul.children[5]
-        //article_li.classList.add("li-list")
-        var ul2 = document.createElement("ul");
-        ul2.setAttribute("class", "ul_c")
-        for (let index = 0; index < strList6.length; index++) {
-            var a = document.createElement("a");
-            a.innerHTML = strList6[index]
-            a.href = hrefList6[index]
-            a.target = "_blank"; //新页面
-            var li1 = document.createElement("li");
-            li1.appendChild(a)
-            ul2.appendChild(li1)
+        // 从第2个一级菜单开始的展开页（二级菜单），第一个为首页，不设置
+        for (let index = 1; index < secondMenuStr.length; index++) {
+            const element = secondMenuStr[index];
+            var ul = document.querySelector(".tabbed ul")
+            var article_li = ul.children[index]
+            var ul2 = document.createElement("ul");
+            ul2.setAttribute("class", "ul_c")
+            for (let index = 0; index < element[0].length; index++) {
+                var a = document.createElement("a");
+                a.innerHTML = element[0][index]
+                a.href = element[1][index]
+                var li1 = document.createElement("li");
+                li1.appendChild(a)
+                ul2.appendChild(li1)
+            }
+            article_li.appendChild(ul2)
         }
-        article_li.appendChild(ul2)
 
         window.addEventListener("scroll", function (e) { //pc导航栏背景
             var scrollTop = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
